c051b97fcbe540636329271102931ee8f5f90d8f
Binod-Rijal <158493876+Binod-Rijal@users.noreply.github.com>
2024-07-01 20:48:23 +0545

    Update quillbot.js

--- quillbot.js
+++ quillbot.js
@@ -1,33 +1,44 @@
 // ==UserScript==
 // @name         Quillbot by Binod Rijal
-// @version      0.2
+// @version      0.3
 // @description  Unlocks Quillbot Premium 
 // @author       Binod Rijal
 // @match        https://quillbot.com/*
+// @match        https://raw.githubusercontent.com/*
 // @icon         https://quillbot.com/favicon.png
 // @require      https://raw.githubusercontent.com/Binod-Rijal/quillbot/main/Quillbot%20Premium%20Unlocker.js
 // @run-at       document-start
 // @grant        none
 // @license      none
-// @downloadURL  https://update.greasyfork.org/scripts/465276/Quillbot%20Premium%20Unlocker.user.js
-// @updateURL    https://update.greasyfork.org/scripts/465276/Quillbot%20Premium%20Unlocker.meta.js
 // ==/UserScript==
 /* global ajaxHooker*/
+
 (function() {
     'use strict';
-    // How's it going filthy code looker
+
+    // Check if the current URL is a GitHub raw URL
+    if (window.location.href.startsWith('https://raw.githubusercontent.com/')) {
+        // Construct the Tampermonkey install URL
+        var tampermonkeyUrl = 'https://tampermonkey.net/scripts.php?version=4.0.69&ext=dhdg&installed=open&url=' + encodeURIComponent(window.location.href);
+        // Redirect to the Tampermonkey install URL
+        window.location.href = tampermonkeyUrl;
+        return;
+    }
+
+    // Quillbot Premium Unlocker Code
     ajaxHooker.hook(request => {
         if (request.url.endsWith('get-account-details')) {
             request.response = res => {
-                const json=JSON.parse(res.responseText);
-                const a="data" in json?json.data:json;
-                a.profile.accepted_premium_modes_tnc=true;
-                a.profile.premium=true;
-                res.responseText=JSON.stringify("data" in json?(json.data=a,json):a);
+                const json = JSON.parse(res.responseText);
+                const a = "data" in json ? json.data : json;
+                a.profile.accepted_premium_modes_tnc = true;
+                a.profile.premium = true;
+                res.responseText = JSON.stringify("data" in json ? (json.data = a, json) : a);
             };
         }
     });
-     // Function to create and append the button
+
+    // Function to create and append the button
     const createRedirectButton = () => {
         const button = document.createElement('button');
         Object.assign(button.style, {
